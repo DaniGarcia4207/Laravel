3e28f1bf92855a903e509b93eb24499f2ce381f7
DaniGarcia4207 <dglr628@gmail.com>
2024-10-08 16:23:36 -0500

    crud-restApi

--- laravel_Breeze/routes/api.php
+++ laravel_Breeze/routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControladorUsuarios;
+use App\Http\Controllers\ControladorCliente;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/usuarios',[ControladorUsuarios::class,'control']);
+Route::get('/clientes',[ControladorCliente::class, 'lista']); 
+Route::get('/clientes/{id}',[ControladorCliente::class, 'obtener']); 
+Route::post('/clientes',[ControladorCliente::class, 'crear']); 
+Route::put('/clientes/{id}',[ControladorCliente::class, 'actualizar']); 
+Route::delete('/clientes/{id}',[ControladorCliente::class, 'eliminar']);
+Route::patch('/clientes/{id}',[ControladorCliente::class, 'actualizar1']);
